713752efb3b5eaf9459848d3e4a4a06cb5a44a49
poonthendral1475 <poonthendral1475@gmail.com>
2025-04-24 05:11:35 +0000

    Finishes Static Page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
+    @name="poonthendral"
+    @time=Time.now
   end
 
   def help
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,36 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  def setup
+    @base_title = "Ruby on Rails Tutorial for Sample App"
+  end
+  def assert_static_pages(url)
+    get url
     assert_response :success
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get home" do
+    assert_static_pages(static_pages_home_url)
+    assert_select "title" , "Home | #{@base_title}"
+  end
+
   test "should get help" do
-    get static_pages_help_url
+    assert_static_pages(static_pages_help_url)
+    assert_select "title" , "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    assert_static_pages(static_pages_about_url)
+    assert_select "title" , "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
     assert_response :success
   end
 end
